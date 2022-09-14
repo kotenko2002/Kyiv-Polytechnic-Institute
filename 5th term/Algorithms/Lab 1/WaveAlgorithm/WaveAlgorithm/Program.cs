f784e9f41c327d2bae822f6cef4c1a808e490990
Dmytro Kotenko <dmytro.ko@qualer.com>
2022-09-14 20:01:17 +0300

    fix laba1 + start alg

---
+++ 5th term/Algorithms/Lab 1/WaveAlgorithm/WaveAlgorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WaveAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = Constants.matrix1;
+
+            PrintMatrix(matrix);
+        }
+
+        public static void Algorithm()
+        {
+
+        }
+
+        public static void PrintMatrix(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] == ' ') 
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    else if(matrix[i,j] == '%')
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    else if (matrix[i, j] == 'S')
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.BackgroundColor = ConsoleColor.White;
+
+                    Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        
+    }
+}
