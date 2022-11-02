5f69bc5cc0d4b3d293745096e284a4a5db76260b
Dmytro Kotenko <dmytro.ko@qualer.com>
2022-11-02 15:23:49 +0200

    lab4 Dijkstra alg

---
+++ 5th term/Algorithms/Laba 4.1/Dijkstra/Dijkstra/Algorithm.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Dijkstra.GraphItems;
+using System.Linq;
+using System;
+
+namespace Dijkstra
+{
+    public static class Algorithm
+    {
+        public static Vertex[] Dijkstra(Graph graph, Vertex start)
+        {
+            Vertex[] vertexArray = graph.GetAllVertexesList().ToArray();
+
+            Vertex curentVertex = start;
+            curentVertex.Value = 0;
+
+            while (vertexArray.Any(item => item.IsVisited == false))
+            {
+                int minValue = int.MaxValue;
+                for (int i = 0; i < vertexArray.Length; i++)
+                {
+                    if (vertexArray[i].Value < minValue && !vertexArray[i].IsVisited)
+                    {
+                        minValue = vertexArray[i].Value;
+                        curentVertex = vertexArray[i];
+                    }
+                }
+
+                if (curentVertex == start && start.IsVisited)
+                    break;
+
+                List<Vertex> listOfVertexes = graph.GetVertexListWithoutVisuted(curentVertex);
+
+                for (int i = 0; i < listOfVertexes.Count; i++)
+                {
+                    int weight = graph.GetWeightByVertexes(curentVertex, listOfVertexes[i]);
+                    int index = Array.FindIndex(vertexArray, item => item == listOfVertexes[i]);
+
+                    if (curentVertex == start)
+                        vertexArray[index].Value = weight;
+                    else if (vertexArray[index].Value > curentVertex.Value + weight)
+                        vertexArray[index].Value = curentVertex.Value + weight;
+                }
+                curentVertex.IsVisited = true;
+                curentVertex = start;
+            }
+            return vertexArray;
+        }
+    }
+}
---
+++ 5th term/Algorithms/Laba 4.1/Dijkstra/Dijkstra/Program.cs
@@ -0,0 +1,109 @@
+﻿using Dijkstra.GraphItems;
+using System;
+
+namespace Dijkstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+
+            var v1 = new Vertex(1);
+            var v2 = new Vertex(2);
+            var v3 = new Vertex(3);
+            var v4 = new Vertex(4);
+            var v5 = new Vertex(5);
+            var v6 = new Vertex(6);
+            var v7 = new Vertex(7);
+            var v8 = new Vertex(8);
+
+            graph.AddVertex(v1);
+            graph.AddVertex(v2);
+            graph.AddVertex(v3);
+            graph.AddVertex(v4);
+            graph.AddVertex(v5);
+            graph.AddVertex(v6);
+            graph.AddVertex(v7);
+            graph.AddVertex(v8);
+
+            graph.AddEdge(v2, v5, 4);
+            graph.AddEdge(v2, v8, 4);
+            graph.AddEdge(v2, v3, 3);
+            graph.AddEdge(v2, v4, 1);
+            graph.AddEdge(v3, v4, 3);
+            graph.AddEdge(v3, v5, 2);
+            graph.AddEdge(v4, v2, 3);
+            graph.AddEdge(v4, v7, 3);
+            graph.AddEdge(v5, v6, 5);
+            graph.AddEdge(v5, v7, 2);
+            graph.AddEdge(v6, v2, 1);
+            graph.AddEdge(v6, v3, 3);
+            graph.AddEdge(v6, v5, 2);
+            graph.AddEdge(v6, v7, 1);
+            graph.AddEdge(v6, v8, 3);
+            graph.AddEdge(v7, v6, 2);
+            graph.AddEdge(v7, v2, 1);
+            graph.AddEdge(v8, v3, 2);
+            graph.AddEdge(v8, v4, 2);
+            graph.AddEdge(v8, v7, 4);
+
+            PrintMatrix(graph);
+            Console.WriteLine();
+            PrintVertwxList(graph, v1);
+            PrintVertwxList(graph, v2);
+            PrintVertwxList(graph, v3);
+            PrintVertwxList(graph, v4);
+            PrintVertwxList(graph, v5);
+            PrintVertwxList(graph, v6);
+            PrintVertwxList(graph, v7);
+            PrintVertwxList(graph, v8);
+
+            Console.WriteLine();
+            var result = Algorithm.Dijkstra(graph, v5);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"Вiдстань вiд вершини \"{v2}\" до вершини \"{result[i]}\" - " +
+                    $"{(result[i].Value == int.MaxValue ? "немає шляху" : result[i].Value)}\n");
+            }
+        }
+        private static void PrintMatrix(Graph graph)
+        {
+            var matrix = graph.GetMatrix();
+
+            Console.Write("__");
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                if (i != graph.VertexCount - 1)
+                    Console.Write($"{i + 1}_");
+                else
+                    Console.Write($"{i + 1}");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                Console.Write(i + 1 + "|");
+                for (int j = 0; j < graph.VertexCount; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void PrintVertwxList(Graph graph, Vertex vertex)
+        {
+            Console.Write($"{vertex.Number}: ");
+
+            var vertexes = graph.GetVertexList(vertex);
+            for (int i = 0; i < vertexes.Count; i++)
+            {
+                if (i != vertexes.Count - 1)
+                    Console.Write($"{vertexes[i].Number}, ");
+                else
+                    Console.Write($"{vertexes[i].Number} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
